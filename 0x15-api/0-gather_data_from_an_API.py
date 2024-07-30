aeed08928037ed74e8537ca3d13d1dcd35c48c88
Parsitau-michael <parsitaumichael@gmail.com>
2024-07-30 03:49:06 -0700

    Fixed Documentation errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,45 @@
 #!/usr/bin/python3
-"""This module represents a python script that uses REST API
+"""
+
+This module represents a Python script that uses REST API to fetch and display
+the TODO list progress of an employee.
 """
 
 
 import requests
-from sys import argv
+from sys import argv, exit
 
 
-if len(argv) != 2:
-    exit()
+def main():
+    """This function defines how to fetch data from an API
+    """
+    if len(argv) != 2:
+        exit(1)
 
-emp_id = int(argv[1])
+    emp_id = int(argv[1])
 
-"""Fetch done tasks"""
-api_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(emp_id)
-response = requests.get(api_url)
-todos = response.json()
+    # Fetch done tasks
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(emp_id)
+    response = requests.get(url)
+    todos = response.json()
 
-total_tasks = len(todos)
-done_tasks = [todo for todo in todos if todo['completed']]
-total_done = len(done_tasks)
+    total_tasks = len(todos)
+    done_tasks = [todo for todo in todos if todo['completed']]
+    total_done = len(done_tasks)
 
-"""Fetch name"""
-usr_url = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
-user_resp = requests.get(usr_url)
-user = user_resp.json()
-name = user['name']
+    # Fetch name
+    usr_url = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
+    user_resp = requests.get(usr_url)
+    user = user_resp.json()
+    name = user['name']
 
-"""Printing progress"""
-print("Employee {} is done with tasks({}/{}):"
-      .format(name, total_done, total_tasks))
-for task in done_tasks:
-    print("\t {}".format(task['title']))
+    # Printing progress
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, total_done, total_tasks))
+
+    for task in done_tasks:
+        print("\t {}".format(task['title']))
+
+
+if __name__ == '__main__':
+    main()
